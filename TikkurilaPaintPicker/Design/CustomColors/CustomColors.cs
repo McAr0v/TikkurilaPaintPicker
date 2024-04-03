ef1e5dddd59e5a350369c8d97543e0bc5fbfae37
McAr0v <dvijkz@gmail.com>
2024-04-03 13:36:01 +0500

    Удалил не нужные цвета

--- TikkurilaPaintPicker/Design/CustomColors/CustomColors.cs
+++ TikkurilaPaintPicker/Design/CustomColors/CustomColors.cs
@@ -3,9 +3,7 @@
     public static class CustomColors
     {
         public static Color TikkurilaRed { get; } = Color.FromArgb("#ED1C24");
-        public static Color YellowLight { get; } = Color.FromArgb("#EDD185");
         public static Color Black { get; } = Color.FromArgb("#000000");
-        public static Color BlackLight { get; } = Color.FromArgb("#141414");
         public static Color Graphite { get; } = Color.FromArgb("#2A2A2A");
         public static Color GreyLight { get; } = Color.FromArgb("#E0DEDB");
         public static Color White { get; } = Color.FromArgb("#ffffff");
